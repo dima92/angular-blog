9aff8a78bb9038777eb83fc2d442667ac1a56ebd
dima- <dima-tkachenko@mail.ru>
2020-11-22 14:53:38 +0300

    Страница входа

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {User} from '../../shared/interfaces';
 
 @Component({
   selector: 'app-login-page',
@@ -7,10 +9,34 @@ import {Component, OnInit} from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
+  // @ts-ignore
+  form: FormGroup;
+
   constructor() {
   }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6)
+      ])
+    });
   }
 
+  submit() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password
+    };
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import {AppComponent} from './app.component';
 import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
 import {HomePageComponent} from './home-page/home-page.component';
 import {PostPageComponent} from './post-page/post-page.component';
-import { PostComponent } from './shared/components/post/post.component';
+import {PostComponent} from './shared/components/post/post.component';
 
 @NgModule({
   declarations: [
