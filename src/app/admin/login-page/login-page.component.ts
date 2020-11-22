4788ab300fdace1203a57f8d3ca27bc06b39d807
dima- <dima-tkachenko@mail.ru>
2020-11-22 15:49:40 +0300

    Сервис для авторизации

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {User} from '../../shared/interfaces';
+import {AuthService} from '../shared/services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -12,7 +14,10 @@ export class LoginPageComponent implements OnInit {
   // @ts-ignore
   form: FormGroup;
 
-  constructor() {
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) {
   }
 
   ngOnInit() {
@@ -37,6 +42,11 @@ export class LoginPageComponent implements OnInit {
       email: this.form.value.email,
       password: this.form.value.password
     };
+
+    this.auth.login(user).subscribe(() => {
+      this.form.reset();
+      this.router.navigate(['/admin', 'dashboard']);
+    });
   }
 }
 
