5d67190bb1424c3cf55005b4fb16969e9aedee9e
dima- <dima-tkachenko@mail.ru>
2020-11-23 18:57:31 +0300

    Форма создания поста

--- src/app/admin/create-page/create-page.component.ts
+++ src/app/admin/create-page/create-page.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Post} from '../../shared/interfaces';
 
 @Component({
   selector: 'app-create-page',
@@ -6,11 +8,31 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./create-page.component.scss']
 })
 export class CreatePageComponent implements OnInit {
+  // @ts-ignore
+  form: FormGroup;
 
   constructor() {
   }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, Validators.required),
+      text: new FormControl(null, Validators.required),
+      author: new FormControl(null, Validators.required)
+    });
+  }
+
+  submit() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const post: Post = {
+      title: this.form.value.title,
+      author: this.form.value.author,
+      text: this.form.value.text,
+      date: new Date()
+    };
   }
 
 }
