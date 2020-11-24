305d2273cf89d53a3ac9bd24780d672916f7cc8e
dima- <dima-tkachenko@mail.ru>
2020-11-25 02:50:19 +0300

    Отображение поста

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,4 +1,9 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {PostsService} from '../shared/posts.service';
+import {Observable} from 'rxjs';
+import {Post} from '../shared/interfaces';
+import {switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-post-page',
@@ -7,10 +12,19 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class PostPageComponent implements OnInit {
 
-  constructor() {
+  post$: Observable<Post>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostsService
+  ) {
   }
 
   ngOnInit() {
+    this.post$ = this.route.params
+      .pipe(switchMap((params: Params) => {
+        return this.postService.getById(params['id']);
+      }));
   }
 
 }
