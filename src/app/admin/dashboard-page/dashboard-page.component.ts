eb036d957f8046ebb7b21befc928d50ed4bfd546
dima- <dima-tkachenko@mail.ru>
2020-11-24 13:04:14 +0300

    Вывод списка постов

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,16 +1,35 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {PostsService} from '../../shared/posts.service';
+import {Post} from '../../shared/interfaces';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  constructor() {
+  posts: Post[];
+  pSub: Subscription;
+
+  constructor(private postService: PostsService) {
   }
 
   ngOnInit() {
+    this.pSub = this.postService.getAll().subscribe(posts => {
+      this.posts = posts;
+    });
+  }
+
+  remove(id: string) {
+
+  }
+
+  ngOnDestroy() {
+    if (this.pSub) {
+      this.pSub.unsubscribe();
+    }
   }
 
 }
